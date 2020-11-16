69abed20b0b9ac472b932e85b76333de3e0f7cb5
Maurício Alexandre Soares Fernandes <mauriciosfernandes94@gmail.com>
2020-11-16 16:18:40 -0300

    Primeiro commit

---
+++ buscar-cursos.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Alura\BuscadorDeCursos\Buscador;
+
+$client = new Client(['base_uri'=> 'https://www.alura.com.br/' ]);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador -> buscar('/cursos-online-programacao/python');
+foreach ($cursos as $curso){
+    echo $curso .PHP_EOL;
+}
